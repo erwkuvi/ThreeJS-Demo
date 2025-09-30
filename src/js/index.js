00895c3aefb4bca170654d0f7b6f5a290e2f7345
Erwin K <e.kuechel@gmail.com>
2025-10-01 00:11:08 +0200

    demo threejs

---
+++ src/js/index.js
@@ -0,0 +1,128 @@
+﻿import * as THREE from 'three';
+import './noise.js';
+
+const vshader = ` 
+varying vec2 v_uv;
+varying vec3 v_position;
+
+void main(){
+    v_uv = uv;
+    v_position = position;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position * 0.25, 1.0 );
+}`;
+
+const fshader = `
+    #include <noise>
+    uniform vec2 u_resolution;
+    uniform vec2 u_mouse;
+    uniform vec3 u_color;
+    uniform vec3 u_color_a;
+    uniform vec3 u_color_b;
+    uniform float u_time;
+    uniform vec3 u_LightColor;
+    uniform vec3 u_DarkColor ;
+    uniform float u_Frequency ;
+    uniform float u_NoiseScale;
+    uniform float u_RingScale ;
+    uniform float u_Contrast ;
+    
+    varying vec2 v_uv;
+    varying vec3 v_position;
+
+    void main(){
+    float n = snoise(v_position);
+    float ring = fract(u_NoiseScale * n);
+    ring *= u_Contrast * (1.0 - ring);
+    float lerp = pow(ring, u_RingScale) + n;
+    vec3 color = mix(u_DarkColor, u_LightColor, lerp);
+ 
+    vec2 center = vec2(0.5);
+    float inRect = circle(v_position.xy, vec2(0.0), 0.5, 0.02);
+    vec2 uv_fract = fract(v_uv * 10.0);
+    vec2 st = v_uv;
+
+    gl_FragColor = vec4(color, 1.0);
+}
+`;
+
+const scene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(1, -1, 1, -1, 0.1, 10);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const clock = new THREE.Clock();
+
+const geometry = new THREE.PlaneGeometry(2, 2);
+
+const uniforms = {
+    u_time: {value: 0.0},
+    u_mouse: {value: {x: 0.0, y: 0.0}},
+    u_color: {value: new THREE.Color(0x00ff00)},
+    u_color_a: { value: new THREE.Color(0xff0000) },
+    u_color_b: { value: new THREE.Color(0xffff00) },
+    u_resolution : { value: new THREE.Vector2() },
+    u_LightColor : { value: new THREE.Color(0xbb905d) },
+    u_DarkColor : { value: new THREE.Color(0x7d490b) },
+    u_Frequency : { value: 2.0 },
+    u_NoiseScale : { value: 6.0 },
+    u_RingScale : { value: 0.2 },
+    u_Contrast : { value: 4.0 },
+};
+const material = new THREE.ShaderMaterial(
+    {
+        uniforms: uniforms,
+        vertexShader: vshader,
+        fragmentShader: fshader
+    }
+);
+const plane = new THREE.Mesh(geometry, material);
+
+scene.add(plane);
+
+camera.position.z = 1;
+
+if ('ontouchstart' in window){
+    document.addEventListener('touchmove', move);
+}
+else{
+    window.addEventListener("resize", onWindowResize, false);
+    document.addEventListener('mousemove', move);
+}
+
+
+onWindowResize();
+animate();
+
+function move(evt) {
+    uniforms.u_mouse.value.x = (evt.touches) ? evt.touches[0].clientX : evt.clientX;
+    uniforms.u_mouse.value.y = (evt.touches)? evt.touches[0].clientY : evt.clientY;
+}
+//End of your code
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+    uniforms.u_time.value = clock.getElapsedTime();
+}
+
+function onWindowResize( event ) {
+    const aspectRatio = window.innerWidth/window.innerHeight;
+    let width, height;
+    if (aspectRatio>=1){
+        width = 1;
+        height = (window.innerHeight/window.innerWidth) * width;
+    }else{
+        width = aspectRatio;
+        height = 1;
+    }
+    camera.left = -width;
+    camera.right = width;
+    camera.top = height;
+    camera.bottom = -height;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    if (uniforms.u_resolution !== undefined){
+        uniforms.u_resolution.value.x = window.innerWidth;
+        uniforms.u_resolution.value.y = window.innerHeight;
+    }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,26 @@
 import * as THREE from 'three';
+import './noise.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import CustomShaderMaterial from 'three-custom-shader-material/vanilla';
+
 
 let scene, camera, renderer, cube, controls, model;
 
+const vshader = `
+
+void main(){
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}`;
+
+const fshader = `
+    void main(){
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    }
+`;
+
 scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0,0,5);
 
@@ -38,6 +54,12 @@ loader.load('../assets/leg.glb',
         model = gltf.scene;
         scene.add(model);
         console.log(model);
+        // store original materials
+        model.traverse((child) =>{
+            if (child.isMesh){
+                child.userData.originalMaterial = child.material.clone();
+            }
+        });
     },
     function ( xhr ) {
         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
@@ -48,10 +70,95 @@ loader.load('../assets/leg.glb',
     }
 );
 
+const uniforms = {
+    u_color_a: { value: new THREE.Color(0xff0000) },
+    u_color_b: { value: new THREE.Color(0xffff00) },
+    u_resolution : { value: new THREE.Vector2() },
+    u_LightColor : { value: new THREE.Color(0xbb905d) },
+    u_DarkColor : { value: new THREE.Color(0x7d490b) },
+    u_Frequency : { value: 2.0 },
+    u_NoiseScale : { value: 6.0 },
+    u_RingScale : { value: 0.2 },
+    u_Contrast : { value: 4.0 },
+};
 //Materials
-const material_01 = new THREE.MeshPhongMaterial( { color: 0x00ff00, side: THREE.DoubleSide} );
+const material_03 = new THREE.MeshStandardMaterial({
+    wireframe: true,
+    wireframeLinewidth: 0.5,
+    color: 0x000000,
+    metalness: 0.0,
+    roughness: 1.0,
+    // side: THREE.DoubleSide
+});
+
+const material_00 = new CustomShaderMaterial({
+    baseMaterial: THREE.MeshPhysicalMaterial,
+    color: new THREE.Color(0x7d490b),
+    // metalness: 0.6,
+    roughness: 0.4,
+    uniforms : {
+        u_resolution : { value: new THREE.Vector2() },
+        u_LightColor : { value: new THREE.Color(0xbb905d) },
+        u_DarkColor : { value: new THREE.Color(0x7d490b) },
+        u_Frequency : { value: 2.0 },
+        u_NoiseScale : { value: 6.0 },
+        u_RingScale : { value: 0.2 },
+        u_Contrast : { value: 4.0 },
+    },
+    fragmentShader: document.getElementById('fragmentshader').textContent,
+    vertexShader: document.getElementById('vertexshader').textContent
+});
+
+const material_01 = new THREE.MeshStandardMaterial({ color: 0xffffff });
+
+material_01.onBeforeCompile = (shader) => {
+    // Add uniforms
+    shader.uniforms.u_LightColor = { value: new THREE.Color(0.73, 0.56, 0.36) };
+    shader.uniforms.u_DarkColor = { value: new THREE.Color(0.49, 0.28, 0.04) };
+    shader.uniforms.u_Frequency = { value: 2.0 };
+    shader.uniforms.u_NoiseScale = { value: 6.0 };
+    shader.uniforms.u_RingScale = { value: 0.2 };
+    shader.uniforms.u_Contrast = { value: 4.0 };
+
+    // Inject noise include
+    shader.fragmentShader = shader.fragmentShader.replace(
+        '#include <common>',
+        `
+    #include <common>
+    #include <noise>
+    uniform vec3 u_LightColor;
+    uniform vec3 u_DarkColor;
+    uniform float u_Frequency;
+    uniform float u_NoiseScale;
+    uniform float u_RingScale;
+    uniform float u_Contrast;
+    `
+    );
+
+    // Insert noise logic inside main()
+    shader.fragmentShader = shader.fragmentShader.replace(
+        '#include <dithering_fragment>',
+        `
+    // --- custom noise logic ---
+    float n = snoise(vViewPosition * u_Frequency);
+    float ring = fract(u_NoiseScale * n);
+    ring *= u_Contrast * (1.0 - ring);
+    float lerpValue = pow(ring, u_RingScale) + n;
+    vec3 noiseColor = mix(u_DarkColor, u_LightColor, lerpValue);
+
+    //csm_DiffuseColor = vec4(noiseColor, 1.0);
+    //diffuseColor.rgb = mix(diffuseColor.rgb, noiseColor, 0.9);
+    gl_FragColor = vec4(noiseColor.rgb, 1.0);
+
+    #include <dithering_fragment>
+    `
+    );
+
+    material_01.userData.shader = shader;
+};
+
 const material_02 = new THREE.MeshPhongMaterial( { color: 0xff0000, side: THREE.DoubleSide} );
-const material_03 = new THREE.MeshPhongMaterial( { color: 0x0000ff, side: THREE.DoubleSide} );
+//const material_03 = new THREE.MeshPhongMaterial( { color: 0x0000ff, side: THREE.DoubleSide} );
 
 //Cube creation
 //const geometry = new THREE.BoxGeometry( 2, 2, 2 );
@@ -63,17 +170,39 @@ const material_03 = new THREE.MeshPhongMaterial( { color: 0x0000ff, side: THREE.
 //console.log(model);
 
 //Button Events
-document.getElementById('mat1').onclick = () => applyMaterial(material_01);
-document.getElementById('mat2').onclick = () => applyMaterial(material_02);
-document.getElementById('mat3').onclick = () => applyMaterial(material_03);
+document.getElementById('mat1').onclick = () => applyMaterialToMeshByName(names, material_00);
+document.getElementById('mat2').onclick = () => applyMaterialToMeshByName(names, material_03);
+// document.getElementById('mat2').onclick = () => applyMaterial(material_03);
+document.getElementById('mat3').onclick = () => restoreOriginalMaterials();
 
+//function to reset original materials
+function restoreOriginalMaterials(){
+    if(!model) return;
+    model.traverse((child) =>{
+        if (child.isMesh && child.userData.originalMaterial){
+            child.material = child.userData.originalMaterial;
+        }
+    })
+}
+const names = ['defaultMaterial_10', 'defaultMaterial'];
 //Apply material function
 function applyMaterial(material){
     if (!model) return;
+
     model.traverse((child) => {
         if (child.isMesh) child.material = material;
     })
 }
+function applyMaterialToMeshByName(meshNames, material) {
+    if (!model) return;
+    const namesSet = new Set(meshNames);
+    model.traverse((child) => {
+        if (child.isMesh && namesSet.has(child.name)){
+          child.material = material;
+        }
+    })
+}
+
 
 //Resize
 function onWindowResize()
