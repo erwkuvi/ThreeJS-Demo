2517c58d48da01ac1300dbc719291db542329c29
Erwin K <e.kuechel@gmail.com>
2025-10-15 16:54:53 +0200

    adding reverse kinematics

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import CustomShaderMaterial from 'three-custom-shader-material/vanilla'; // Allows to use MeshPhysicalMaterial as base
 import { CCDIKSolver } from 'three/addons/animation/CCDIKSolver.js';
-import {Skeleton} from "three";
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import {TransformControls} from "three/addons";
+
 
 let scene, camera, renderer, cube, controls, model;
 
@@ -34,9 +36,6 @@ renderer = new THREE.WebGLRenderer({
 );
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-//Controls
-controls = new OrbitControls( camera, renderer.domElement );
-controls.update();
 
 //Light
 //const light = new THREE.DirectionalLight(0xffffff, 1.2);
@@ -56,92 +55,71 @@ const loader = new GLTFLoader();
 //dracoLoader.setDecoderPath( '../assets/leg_prosthesis-gltf' );
 //loader.setDRACOLoader( dracoLoader );
 
-const gltf = await loader.loadAsync('../assets/prothese-rigging2.glb');
+const gltf = await loader.loadAsync('../assets/prothese-rigging3.glb');
 
 model = gltf.scene;
+let skinnedMeshes = [];
+console.log(model);
+model.traverse((obj) =>
+    {
+        if (obj.isSkinnedMesh){
+            skinnedMeshes[obj.name] = obj;
+            console.log(obj.type, obj.name);
+        }
+    });
 scene.add(model);
-console.log("model", model);
-let skinnedMesh;
-model.traverse((obj) => { if (obj.isSkinnedMesh) skinnedMesh = obj; });
-//console.log(skinnedMesh);
-if (!skinnedMesh) console.error("No SkinnedMesh found in loaded model!");
-const bones = skinnedMesh.skeleton.bones;
-// console.log("Bones indices: ", bones.map((b, i) => ({ i, name: b.name })));
-// model.traverse((obj) => { console.log(obj.type, obj.name);});
-// store original materials
-model.traverse((child) => { if (child.isMesh){ child.userData.originalMaterial = child.material.clone();} });
+// console.log(skinnedMeshes.foot.type, skinnedMeshes.FootPiece.name, targetPosition);
+if (!skinnedMeshes.QuadPiece) {
+    console.error("No SkinnedMesh found in loaded model!");
+} else {
+    const bones = skinnedMeshes.QuadPiece.skeleton.bones; // Use the first one, or iterate if needed
+    bones.forEach((b, i) => console.log(i, b.name));
+    const maxIndex = bones.length - 1;
+}
+const targetPosition = skinnedMeshes.QuadPiece.skeleton.bones[3].getWorldPosition(new THREE.Vector3());
 
+
+// skinnedMeshes.foot.add(skinnedMeshes.foot.skeleton.bones[0]); // add root bone to skinned mesh to make it work
 const iks = [
     {
-        target: 5, // "target"
-        effector: 4, // "bone3"
-        links: [ { index: 3 }, { index: 2 }, { index: 1 } ] // "bone2", "bone1", "bone0"
+        target: 3, // "target"
+        effector: 1,
+        links: [
+            { index: 0},
+        ] // "bone2", "bone1", "bone0"
     }
 ];
-console.log("skinnedMesh", skinnedMesh);
-let ikSolver = new CCDIKSolver(skinnedMesh, iks);
-//let ccdikhelper = new THREE.CCDIKHelper(model, iks, 0.01);
 
-// loader.load('../assets/prothese-rigging2.glb',
-//         function (gltf)
-//         {
-//             model = gltf.scene;
-//             scene.add(model);
-//             console.log("model", model);
-//             let skinnedMesh;
-//             model.traverse((obj) => { if (obj.isSkinnedMesh) skinnedMesh = obj; });
-//             //console.log(skinnedMesh);
-//             if (!skinnedMesh)
-//             {
-//                 console.error("No SkinnedMesh found in loaded model!");
-//                 return;
-//             }
-//             const bones = skinnedMesh.skeleton.bones;
-//             model.traverse((obj) => { console.log(obj.type, obj.name);});
-//
-//             // store original materials
-//             model.traverse((child) =>
-//             {
-//                 if (child.isMesh){ child.userData.originalMaterial = child.material.clone();}
-//             });
-//             // const iks = [
-//             //     {
-//             //         target: 5, // "target"
-//             //         effector: 4, // "bone3"
-//             //         links: [ { index: 3 }, { index: 2 }, { index: 1 } ] // "bone2", "bone1", "bone0"
-//             //     }
-//             // ];
-//             //
-//             // let ikSolver;
-//             // ikSolver = new CCDIKSolver(model, iks);
-//             //console.log("Bones indices: ", bones.map((b, i) => ({ i, name: b.name })));
-//         },
-//     function ( xhr )
-//         {
-//         console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
-//         },
-//     function ( error )
-//         {
-//             console.log( 'An error happened.' );
-//         }
-//     );
+let ikSolver = new CCDIKSolver(skinnedMeshes.QuadPiece, iks);
+let helper = ikSolver.createHelper(0.01);
+scene.add(helper);
 
-//IK setup
-// the order of bones is important here, the first bone is the one that will be rotated first
-// the last bone in the chain should be the one that is the parent of the effector
-// indices are based on the order of bones in the model, you can log them to find out
-// in this example, we have a chain of 3 bones, "bone0", "bone1", "bone2" and an effector "bone3"
-// the target is a separate object that the effector will try to reach
-// in this case, the target is "target", which is a child of the root of the model
-// bone0 -> bone1 -> bone2 -> bone3 (effector) -> target
-// indices: 0 -> 1 -> 2 -> 3 (effector) -> 5 (target)
-// the IK solver will try to move bone2, bone1 and bone0 to reach the target with bone3
-// you can adjust the number of iterations and the min/max angle for more realistic movements
-// minAngle and maxAngle are in radians
-
-//// if the model has animations, you can set up the IK solver like this
+let conf = {
+    ik_solver: true,
+    update: updateIK
+};
+let gui = new GUI();
+gui.add( conf, 'ik_solver' ).name( 'IK auto update' );
+gui.add( conf, 'update' ).name( 'IK manual update()' );
+gui.open();
 
 
+let transformControls = new TransformControls( camera, renderer.domElement );
+transformControls.size = 0.35;
+transformControls.showX = false;
+transformControls.space = 'world';
+transformControls.attach( skinnedMeshes.FootPiece);
+scene.add( transformControls.getHelper() );
+
+//Controls
+controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 5;
+controls.maxDistance = 10;
+controls.enableDamping = true;
+// controls.target.copy( targetPosition );
+controls.update();
+
+model.traverse((child) => { if (child.isMesh){ child.userData.originalMaterial = child.material.clone();} });
 const uniforms = {
     u_color_a: { value: new THREE.Color(0xff0000) },
     u_color_b: { value: new THREE.Color(0xffff00) },
@@ -266,7 +244,17 @@ function applyMaterialToMeshByName(meshNames, material) {
     })
 }
 
+function updateIK() {
 
+    if ( IKSolver ) IKSolver.update();
+
+    scene.traverse( function ( object ) {
+
+        if ( object.isSkinnedMesh ) object.computeBoundingSphere();
+
+    } );
+
+}
 //Resize
 function onWindowResize()
 {
@@ -277,7 +265,7 @@ function onWindowResize()
 
 window.addEventListener('resize', onWindowResize);
 
-controls.target.set(0, 2.5, 0);
+controls.target.copy(targetPosition);
 //Animation loop
 function animate()
 {
